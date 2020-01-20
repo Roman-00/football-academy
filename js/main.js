f13006b1c2544f536465b5c21d2aa52a04d5a73e
Roman00 <rvasin00@gmail.com>
2020-01-21 00:07:33 +0600

    feat: modal seven junior

--- js/main.js
+++ js/main.js
@@ -48,6 +48,50 @@ $(document).ready(function () {
     modalIntensive.toggleClass('modal-intensive--visible');
   });
 
+  //Четвертая модалка
+  var modalBaby = $('.modal-baby'),
+      modalBtn = $('[data-toggle=modal-baby]'),
+      closeBtn = $('.modal-baby__close');
+  modalBtn.on('click', function () {
+    modalBaby.toggleClass('modal-baby--visible');
+    });
+  closeBtn.on('click', function() {
+    modalBaby.toggleClass('modal-baby--visible');
+  });
+
+  //Пятая модалка
+  var modalJuvinali = $('.modal-juvenali'),
+      modalBtn = $('[data-toggle=modal-juvenali]'),
+      closeBtn = $('.modal-juvenali__close');
+  modalBtn.on('click', function () {
+    modalJuvinali.toggleClass('modal-juvenali--visible');
+    });
+  closeBtn.on('click', function() {
+    modalJuvinali.toggleClass('modal-juvenali--visible');
+  });
+
+  //Шестая модалка
+  var modalJuvinaliTwo = $('.modal-juvenali-two'),
+      modalBtn = $('[data-toggle=modal-juvenali-two]'),
+      closeBtn = $('.modal-juvenali-two__close');
+  modalBtn.on('click', function () {
+    modalJuvinaliTwo.toggleClass('modal-juvenali-two--visible');
+    });
+  closeBtn.on('click', function() {
+    modalJuvinaliTwo.toggleClass('modal-juvenali-two--visible');
+  });
+
+  //Шестая Седьмая
+  var modalJunior = $('.modal-junior'),
+      modalBtn = $('[data-toggle=modal-junior]'),
+      closeBtn = $('.modal-junior__close');
+  modalBtn.on('click', function () {
+    modalJunior.toggleClass('modal-junior--visible');
+    });
+  closeBtn.on('click', function() {
+    modalJunior.toggleClass('modal-junior--visible');
+  });
+
   //Маска для телефона
   $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "Ваш номер телефона"});
 
@@ -117,6 +161,222 @@ $(document).ready(function () {
         }
       });
      },
+  });
+
+  //Валидация Формы Baby Star
+  $('.modal-baby__form').validate({
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        maxlength: 15,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило обьект
+    },// сообщения
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не должно быть короче 2 символов",
+        maxlength: "Имя не должно превышать 15 символов"
+      },
+      userPhone: "Номер Обязательно",
+    },
+    errorPlacement: function (error, element) {
+      if (element.attr("type") == "checkbox") {
+          return element.next('label').append(error);
+      }
+  
+       error.insertAfter($(element));
+    },
+    errorElement: "div",
+    errorClass: "invalid",
+
+    // Jquery Ajax form
+    submitHandler: function(form) {
+     $.ajax({
+       type: "POST",
+       url: "send.php",
+       data: $(form).serialize(),
+       success: function (response) {
+        let ownModal = document.getElementById('ownModal');
+        ownModal.classList.add('active-own');
+        const RemoveOwnModal = () => {
+         ownModal.classList.remove('active-own')
+        };
+        setTimeout(RemoveOwnModal, 3000); 
+         $(form)[0].reset();
+         modalBaby.removeClass('modal-beginer--visible');
+       },
+       error: function (response) {
+         console.error('Ошибка запроса! ' + response);
+         ym('65026879', 'reachGoal', 'button'); return true;
+       }
+     });
+    },
+  });
+
+  //Валидация Формы Ювеналы
+  $('.modal-juvenali__form').validate({
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        maxlength: 15,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило обьект
+    },// сообщения
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не должно быть короче 2 символов",
+        maxlength: "Имя не должно превышать 15 символов"
+      },
+      userPhone: "Номер Обязательно",
+    },
+    errorPlacement: function (error, element) {
+      if (element.attr("type") == "checkbox") {
+          return element.next('label').append(error);
+      }
+  
+       error.insertAfter($(element));
+    },
+    errorElement: "div",
+    errorClass: "invalid",
+
+    // Jquery Ajax form
+    submitHandler: function(form) {
+     $.ajax({
+       type: "POST",
+       url: "send.php",
+       data: $(form).serialize(),
+       success: function (response) {
+        let ownModal = document.getElementById('ownModal');
+        ownModal.classList.add('active-own');
+        const RemoveOwnModal = () => {
+         ownModal.classList.remove('active-own')
+        };
+        setTimeout(RemoveOwnModal, 3000); 
+         $(form)[0].reset();
+         modalJuvinali.removeClass('modal-juvenali--visible');
+       },
+       error: function (response) {
+         console.error('Ошибка запроса! ' + response);
+         ym('65026879', 'reachGoal', 'button'); return true;
+       }
+     });
+    },
+  });
+
+  //Валидация Формы Ювеналы 2
+  $('.modal-juvenali-two__form').validate({
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        maxlength: 15,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило обьект
+    },// сообщения
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не должно быть короче 2 символов",
+        maxlength: "Имя не должно превышать 15 символов"
+      },
+      userPhone: "Номер Обязательно",
+    },
+    errorPlacement: function (error, element) {
+      if (element.attr("type") == "checkbox") {
+          return element.next('label').append(error);
+      }
+  
+       error.insertAfter($(element));
+    },
+    errorElement: "div",
+    errorClass: "invalid",
+
+    // Jquery Ajax form
+    submitHandler: function(form) {
+     $.ajax({
+       type: "POST",
+       url: "send.php",
+       data: $(form).serialize(),
+       success: function (response) {
+        let ownModal = document.getElementById('ownModal');
+        ownModal.classList.add('active-own');
+        const RemoveOwnModal = () => {
+         ownModal.classList.remove('active-own')
+        };
+        setTimeout(RemoveOwnModal, 3000); 
+         $(form)[0].reset();
+         modalJuvinaliTwo.removeClass('modal-juvenali-two--visible');
+       },
+       error: function (response) {
+         console.error('Ошибка запроса! ' + response);
+         ym('65026879', 'reachGoal', 'button'); return true;
+       }
+     });
+    },
+  });
+
+  //Валидация Формы Юниоры
+  $('.modal-junior__form').validate({
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        maxlength: 15,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило обьект
+    },// сообщения
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не должно быть короче 2 символов",
+        maxlength: "Имя не должно превышать 15 символов"
+      },
+      userPhone: "Номер Обязательно",
+    },
+    errorPlacement: function (error, element) {
+      if (element.attr("type") == "checkbox") {
+          return element.next('label').append(error);
+      }
+  
+       error.insertAfter($(element));
+    },
+    errorElement: "div",
+    errorClass: "invalid",
+
+    // Jquery Ajax form
+    submitHandler: function(form) {
+     $.ajax({
+       type: "POST",
+       url: "send.php",
+       data: $(form).serialize(),
+       success: function (response) {
+        let ownModal = document.getElementById('ownModal');
+        ownModal.classList.add('active-own');
+        const RemoveOwnModal = () => {
+         ownModal.classList.remove('active-own')
+        };
+        setTimeout(RemoveOwnModal, 3000); 
+         $(form)[0].reset();
+         modalJunior.removeClass('modal-junior--visible');
+       },
+       error: function (response) {
+         console.error('Ошибка запроса! ' + response);
+         ym('65026879', 'reachGoal', 'button'); return true;
+       }
+     });
+    },
   });
 
   //Валидация Формы Beginer
